ec48127f55295b30f81d6be5f92d3bd4e460651d
T. Robert Ward <trironyard@icloud.com>
2016-10-02 18:46:22 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void GoDoAnIntro();
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    GoDoAnIntro();
+
+return 0;
+
+}
+
+    // ++++ Intro ++++
+    void GoDoAnIntro()
+    {
+        cout << "FLIGHT DATA REPORT\n==================\n" << endl;
+    }
